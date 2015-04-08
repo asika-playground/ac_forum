57be8121f9e74c4f79b702e6c5ce6a8096c1b268
asika Kuo <asika.tw@gmail.com>
2015-04-08 16:41:58 +0800

    initialized devise;

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,24 @@
+class TopicsController < ApplicationController
+
+    def index
+        @topics = Topic.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
