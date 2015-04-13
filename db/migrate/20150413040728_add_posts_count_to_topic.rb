d875dcc3f44dff3880725b0a98ffc68ca3f5f7d1
asika Kuo <asika.tw@gmail.com>
2015-04-13 12:13:46 +0800

    add comment count using count cache;

---
+++ db/migrate/20150413040728_add_posts_count_to_topic.rb
@@ -0,0 +1,10 @@
+class AddPostsCountToTopic < ActiveRecord::Migration
+  def change
+
+    add_column :topics, :comments_count, :integer, :default => 0
+
+    Topic.pluck(:id).each do |i|
+      Topic.reset_counters(i, :comments) # 全部重算一次
+    end
+  end
+end
